8940082fd1160c77231ef1c4f39f4be72376b61d
junbox98221 <junbox98221@gmail.com>
2021-09-26 15:45:59 +0900

    [update] 공지사항 세부 페이지 overflow 처리

--- src/pages/NoticeDetail.js
+++ src/pages/NoticeDetail.js
@@ -7,7 +7,7 @@ const Notice = () => {
         date: "2021-01-01",
         main: "안녕하세요. 여러분의 작품을 더 나은 전시장으로 이어주는 링커벨입니다.포트레이드를 이용해 주시는 여러분께 감사의 말씀 드리며, 아래와 같이 사이트의 레이아웃이 일부 변경되오니 참고하시길 바랍니다.",
     };
-    const data = `<h2 class="ql-indent-1">asdfasdfasdfsdfsadf<span style="color: rgb(178, 107, 0);">asdfsadfsadfasdf</span></h2><ul><li><strong>qweqeqe</strong></li><li><strong>asdasd</strong></li></ul><p><strong>asdasd</strong></p>`;
+    const data = `<h2 class="ql-indent-1">asssssssssssssssssssssssssssssssssssssssasdfasdfasdfsdfsadf<span style="color: rgb(178, 107, 0);">asdfsadfsadfasdf</span></h2><ul><li><strong>qweqeqe</strong></li><li><strong>asdasd</strong></li></ul><p><strong>asdasd</strong></p>`;
 
     return (
         <div className="noticeDetail-wrap">
