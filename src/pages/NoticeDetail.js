9ec1d429dd650fa5cd3fcec12466f60a3b21003e
junbox98221 <junbox98221@gmail.com>
2021-09-26 15:38:13 +0900

    [update] 공지 페이지 css 수정, 공지 세부사항 html 수정

--- src/pages/NoticeDetail.js
+++ src/pages/NoticeDetail.js
@@ -1,19 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 import "./css/noticeDetail.css";
-
-const BoardItem = ({ title, num, date }) => {
-    return (
-        <>
-            <div className="noticeDetail-tab-post">
-                <span>{num}</span>
-                <span>{title}</span>
-                <span>{date}</span>
-            </div>
-            <div className="noticeDetail-tab-post-line"></div>
-        </>
-    );
-};
 
 const Notice = () => {
     const boardData = {
@@ -21,6 +7,7 @@ const Notice = () => {
         date: "2021-01-01",
         main: "안녕하세요. 여러분의 작품을 더 나은 전시장으로 이어주는 링커벨입니다.포트레이드를 이용해 주시는 여러분께 감사의 말씀 드리며, 아래와 같이 사이트의 레이아웃이 일부 변경되오니 참고하시길 바랍니다.",
     };
+    const data = `<h2 class="ql-indent-1">asdfasdfasdfsdfsadf<span style="color: rgb(178, 107, 0);">asdfsadfsadfasdf</span></h2><ul><li><strong>qweqeqe</strong></li><li><strong>asdasd</strong></li></ul><p><strong>asdasd</strong></p>`;
 
     return (
         <div className="noticeDetail-wrap">
@@ -34,7 +21,7 @@ const Notice = () => {
                     <span>{boardData.date}</span>
                 </div>
                 <div className="noticeDetail-category-line"></div>
-                <div className="noticeDetail-main">{boardData.main}</div>
+                <div className="noticeDetail-main" dangerouslySetInnerHTML={{ __html: data }}></div>
                 <div className="noticeDetail-category-line"></div>
                 <div className="noticeDetail-tab-category">
                     <span>다음</span>
